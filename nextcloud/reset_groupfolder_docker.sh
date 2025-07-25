e3f5263f8c0f8b01462ff2db72ef2c6a7dccca62
nabim777 <nabinalemagar019@gmail.com>
2025-07-25 11:56:56 +0545

    rename file
    Signed-off-by: nabim777 <nabinalemagar019@gmail.com>

--- nextcloud/reset_groupfolder_docker.sh
+++ nextcloud/reset_groupfolder_docker.sh
@@ -23,7 +23,7 @@
 
 
 # Command to run script:
-#   NEXTCLOUD_CONTAINER_ID=bbe57f6c08ae bash reset_groupfolder.sh
+#   NEXTCLOUD_CONTAINER_ID=bbe57f6c08ae bash reset_groupfolder_locally.sh
 
 
 # helper function
